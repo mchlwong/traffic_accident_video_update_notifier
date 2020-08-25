f0b97498c1b6fdb0c8157b54fe63c099a410c80a
Michael Wong <ofmchl@gmail.com>
2020-08-25 09:09:23 +0800

    change waitUntil to 'domcontentloaded'
    add getVideos error handling

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ const axios = require('axios');
 const { token, chatId } = require('./config.js');
 
 async function main() {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  await page.goto('https://www.acfun.cn/u/4075269', {
-    waitUntil: 'networkidle0',
-  });
-
   let videos = [];
 
   setInterval(async () => {
-    await page.reload({
-      waitUntil: 'networkidle0',
-    });
-    const currentVideos = await getVideos(page);
+    const currentVideos = await getVideos();
     const newVideos = currentVideos.filter(
       v => !videos.map(({ link }) => link).includes(v.link)
     );
@@ -28,23 +19,33 @@ async function main() {
   }, 15 * 60 * 1000);
 }
 
-async function getVideos(page) {
-  const videos = await page.evaluate(() => {
-    const cards = document.querySelectorAll('#ac-space-video-list > a');
-    const videos = [];
-    for (const card of cards) {
-      const link = card.href;
-      const description = card.querySelector(
-        'figure.video > figcaption > p.title.line'
-      ).textContent;
-      videos.push({
-        link,
-        description,
-      });
-    }
+async function getVideos() {
+  try {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto('https://www.acfun.cn/u/4075269', {
+      waitUntil: 'domcontentloaded',
+    });
+    const videos = await page.evaluate(() => {
+      const cards = document.querySelectorAll('#ac-space-video-list > a');
+      const videos = [];
+      for (const card of cards) {
+        const link = card.href;
+        const description = card.querySelector(
+          'figure.video > figcaption > p.title.line'
+        ).textContent;
+        videos.push({
+          link,
+          description,
+        });
+      }
+      return videos;
+    });
+    await browser.close();
     return videos;
-  });
-  return videos;
+  } catch (err) {
+    return [];
+  }
 }
 
 async function sendTelegram(videos) {
